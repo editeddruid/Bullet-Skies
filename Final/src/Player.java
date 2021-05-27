b9919cb57e59a8b287b43b67e06f0947061a303b
John <83822632+editeddruid@users.noreply.github.com>
2021-05-27 07:58:49 -0400

    Add files via upload

--- Final/src/Player.java
+++ Final/src/Player.java
@@ -2,11 +2,18 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 /**
  * 
  * @author John D'Arcy
+ * 
+ * Image code from Brendan Cashman
  *
  */
 
@@ -15,6 +22,7 @@ public class Player extends JComponent
 	//Fields
 	private Rectangle2D.Double player;
 	private int dx, dy, health, screenClears;
+	private BufferedImage image;
 	//Constructor
 	public Player(int x, int y, int health)
 	{
@@ -24,15 +32,20 @@ public class Player extends JComponent
 		dy = 0;
 		this.health = health;
 		screenClears = 5;
+		try {                
+	          image = ImageIO.read(new File("Player.png"));
+	       } catch (IOException ex) {
+	            System.out.println("ERROR");
+	       }
 		setLocation(x,y);
 	}
 	//Painting the Component
 	public void paintComponent(Graphics g)
 	{
 		Graphics2D g2 = (Graphics2D) g;
-		
-		g2.setColor(Color.BLACK);
-		g2.fill(player);
+//		g2.setColor(Color.BLACK);
+//		g2.fill(player);
+		g2.drawImage(image, 0, 0, this);
 	}
 	//Setting dx and dy
 	public void setDx(int x)
